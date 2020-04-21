a8399cecfca3fc9510699ecf86cc6d9ba562ee03
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-04-21 11:01:14 +0200

    Fav stack added

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,68 +1,98 @@
 import React from 'react';
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+//Tab Bars
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+
+//Screens
 import HomeScreen from "../screens/HomeScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
 import FiltersScreen from "../screens/FiltersScreen";
 import MealsDetailScreen from "../screens/MealsDetailScreen";
-
+//Icons
 import { Ionicons } from "@expo/vector-icons";
-import FavoritesButton from '../components/Buttons/FavoritesButton';
+import { Platform } from 'react-native';
 
 
+const defaultNavOptions = 
+{
+        title: "Home",
+        headerTintColor: "white", //Header button colors
+        headerStyle:
+        {
+            backgroundColor: "#badc57", // header bg color
+        },
+        headerTitleStyle:
+        {
+            color: "black", // header font color
+        },
+}
 
+//Stack Navigators
 const MainNavigator = createStackNavigator(
- {
-  Home: HomeScreen,
-  Category: CategoryMealsScreen,
-  
-  Filter: FiltersScreen,
-  Detail: MealsDetailScreen,
- },
- {
-  initialRouteName: "Home",
-  defaultNavigationOptions: {
-   title: "Home",
-   headerTintColor: "white", //Header button colors
-   headerStyle: {
-    backgroundColor: "#badc57", // header bg color
-   },
-   headerTitleStyle: {
-    color: "black", // header font color
-   },
-  },
- }
-);
-
-
-
-const BottomNavigator = createBottomTabNavigator({
- MainNavigator,
- Favorites: FavoritesScreen,
+{
+    Home: HomeScreen,
+    Category: CategoryMealsScreen,
+    Filter: FiltersScreen,
+    Detail: MealsDetailScreen,
 },
+{
+    //initialRouteName: "Home", Takes 1st screen by default
+    defaultNavigationOptions: defaultNavOptions
+});
+ 
+const FavoritesNavigator = createStackNavigator(
+{
+    Favorites: FavoritesScreen,
+    Detail: MealsDetailScreen,
+},
+{
+    defaultNavigationOptions: defaultNavOptions   
+});
+
+
+
+//Bottom Tabs Android | iOS
+const BottomNavigator =
+ Platform.OS === "android"
+    ? createMaterialBottomTabNavigator( //Android
+        {
+            MainNavigator,
+            FavoritesNavigator
+        },
+        {
+            activeColor: 'white',
+            shifting: true
+        })
+    : createBottomTabNavigator( //iOS
+    {
+        MainNavigator, //Meals: MainNavigator same
+        FavoritesNavigator
+    },
     {
         tabBarOptions:
         {
-            activeTintColor: 'orange',
-            //inactiveTintColor:'green'
-        }
-    }
-);
+            activeTintColor: "orange",
+            //activeBackgroundColor:'yellow'
+        },
+    });
 
+//Tab Bar Icon Config
 MainNavigator.navigationOptions = {
- title: "Home",
-    tabBarIcon: <Ionicons name="ios-restaurant" size={20} color='orange'/>,
+    title: "Home",
+    tabBarIcon: <Ionicons name="ios-restaurant" size={20} color="orange" />,
+    //tabBarColor: "lightgreen", //Only for android
 };
 
-FavoritesScreen.navigationOptions = {
- title: "Favorites",
- //tabBarLabel: 'Favorites', same as above
+FavoritesNavigator.navigationOptions = {
+ title: "Favorites", //tabBarLabel: 'Favorites', same
  tabBarIcon: (tabInfo) => {
   //Gets same tint color active/inactive for icon
   return <Ionicons name="ios-star" size={20} color={tabInfo.tintColor} />;
  },
+ tabBarColor: "red", //Only for android
 };
 
 
